8f5414fe041ce16ce985b4c04529ffc94838c585
Oli Matthews <olvado@gmail.com>
2015-02-28 12:43:30 +0000

    recipe CrUd

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,14 +9,39 @@ class RecipesController < ApplicationController
   end
 
   def new
-    @recipe = Recipe.new(user_id: @current_user.id)
+    @recipe = @current_user.recipes.new
+  end
+
+  def create
+    @recipe = @current_user.recipes.new(permitted_params)
+    if @recipe.save
+      redirect_to @current_user, notice: "Recipe created."
+    else
+      render :new
+    end
   end
 
   def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(permitted_params)
+      redirect_to @current_user, notice: "Recipe created."
+    else
+      render :new
+    end
   end
 
 protected
 
   def permitted_params
+    params.require(:recipe).permit(
+        :title,
+        :body,
+        :user_id
+      )
+  end
 
 end
