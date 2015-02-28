2534f50ebc125fe0275dbe79a0616a833f8ec4c2
Oli Matthews <olvado@gmail.com>
2015-02-28 09:00:39 +0000

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,10 @@ user.recipes.create!([
     { title: "Pork tenderloin with mustard, walnuts and creme fraiche" }
   ])
 
-puts User.first.recipes
+FavouriteRecipe.create!([
+    { user_id: user.id, recipe_id: user.recipes[2].id },
+    { user_id: user.id, recipe_id: user.recipes[1].id },
+    { user_id: user.id, recipe_id: user.recipes[0].id }
+  ])
+
+puts User.first.favourites
